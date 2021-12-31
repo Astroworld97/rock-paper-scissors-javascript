35242b302565785ca075884ba73c17903a8884c0
iagonzal97 <iagonzal97@gmail.com>
2021-12-31 18:16:03 -0400

    added play function and how to interpret user input

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log(Math.random())
+let play = function(){
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+      
+      readline.question("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors'\n", name => {
+        //console.log(`Choice is: ${name}!`);
+        readline.close();
+      });
+}
+
+play()
