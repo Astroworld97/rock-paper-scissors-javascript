9fec24a0c35fce50f76577dda67b1824bf2b2938
iagonzal97 <iagonzal97@gmail.com>
2021-12-31 21:12:24 -0400

    finished, but requires debugging of the input function

--- main.js
+++ main.js
@@ -1,13 +1,79 @@
 let play = function(){
+    //playerChoice = input() + ''
+    playerChoice = 'r'
+    playerChoice = playerChoice.toLowerCase()
+    computerChoice = getComputerChoice()
+    retval = [playerChoice, computerChoice]
+    if (playerChoice === computerChoice){
+        retval.push(0)
+    }else if(is_win(playerChoice, computerChoice)){
+        retval.push(1)
+    }else{
+        retval.push(-1)
+    }
+    return retval
+}
+
+let playBestOf = function(n){
+    //play against the computer until someone wins best of n games
+    //to win, you must win ceil(n/2) games (i.e. 2/3, 3/5, 4/7)
+    player_wins = 0
+    computer_wins = 0
+    wins_necessary = Math.ceil(n / 2)
+    while(player_wins<wins_necessary && computer_wins<wins_necessary){
+        arr = play()
+        user = arr[0]
+        computer = arr[1]
+        result = arr[2]
+        if(result === 0){
+            console.log(`Tie. You and the computer have both chosen ${user}.`)
+        }else if(result === 1){
+            player_wins += 1
+            console.log(`You chose ${user} and the computer chose ${computer}. You won!`)
+        }else{
+            computer_wins += 1
+            console.log(`You chose ${user} and the computer chose ${computer}. You lost :(`)
+        }
+        if (player_wins === wins_necessary){
+            console.log(`You have won the best of ${n} games! What a champ :D`)
+            return
+        }
+        if(computer_wins === wins_necessary){
+            console.log(`"Unfortunately, the computer has won the best of ${n} games. Better luck next time!"`)
+            return
+        }
+    }
+}
+
+let is_win = function(player, opponent){
+    // return true if the player beats the opponent
+    // winning conditions: r>s, s>p, p>r
+    if ((player === "r" && opponent === "s") || (player === "s" && opponent === "p") || (player === "p" && opponent === "r")){
+        return true
+    }
+    return false
+}
+
+let input = function(){
     const readline = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
-      });
-      
-      readline.question("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors'\n", name => {
-        //console.log(`Choice is: ${name}!`);
-        readline.close();
+      });  
+      readline.question("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors'\n", playerChoice => {
+      readline.close();
+      return playerChoice
       });
 }
 
-play()
+let getComputerChoice = function(){
+    const choices = ['r', 'p', 's']
+    const num = getRandomInt(3)
+    return choices[num]
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+//main
+playBestOf(5)
